25ad8862bebe44141260c34c6dfbf9be80e9f4f9
buty619 <36422189+buty619@users.noreply.github.com>
2018-08-29 14:04:08 -0500

    commit

--- js/Directives/showApp.js
+++ js/Directives/showApp.js
@@ -9,7 +9,7 @@ app.directive('showApp', function() {
       
             scope.rotar = function() { 
               element.toggleClass('rotar');
-              $('#texto').toggleClass("hide");
+              $(this.$("#texto")).toggleClass("hide");
         }
 
         }
